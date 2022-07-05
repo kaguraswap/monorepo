b642f73b2e95bb779f300800391f2da1dc2ca820
kenta suhara <ponta.blockchain@gmail.com>
2022-07-05 16:54:16 +0900

    style: asset detail mock

--- packages/web/src/components/organisms/AssetDetail/AssetDetail.tsx
+++ packages/web/src/components/organisms/AssetDetail/AssetDetail.tsx
@@ -1,5 +1,6 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Icon, Image, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
+import { FiClock, FiHeart } from "react-icons/fi";
 
 import { Asset } from "../../../../../common/types/asset";
 import { Link } from "../../atoms/Link";
@@ -11,21 +12,64 @@ export interface AssetDetailProps {
 export const AssetDetail: React.FC<AssetDetailProps> = ({ asset }) => {
   return (
     <Box>
-      <Text>{asset.chainId}</Text>
-      <Text>{asset.contractAddress}</Text>
-      <Text>{asset.tokenId}</Text>
-      {asset.collection && (
-        <Box>
-          <Text>
-            <Link href={`/collections/${asset.collection.chainId}/${asset.collection.contractAddress}`}>
-              {asset.collection.name}
-            </Link>
-          </Text>
-        </Box>
-      )}
-      <Text>{asset.name}</Text>
-      <Text>{asset.description}</Text>
-      <Text>{asset.image}</Text>
+      <Box maxW="7xl" mx="auto" px={{ base: "4", md: "8", lg: "12" }} py={{ base: "6", md: "8", lg: "12" }}>
+        <Stack direction={{ base: "column", lg: "row" }} spacing={{ base: "6", lg: "12", xl: "16" }}>
+          <Image src={asset.image} alt={asset.name}></Image>
+          <Stack spacing={{ base: "6", lg: "8" }} maxW={{ lg: "sm" }} justify="center">
+            <Stack spacing={{ base: "3", md: "4" }}>
+              <Stack spacing="3">
+                <Text fontSize="sm" fontWeight="medium" color={useColorModeValue("gray.600", "gray.400")}>
+                  {asset.chainId}
+                </Text>
+                <HStack alignSelf="baseline">
+                  {/* <Rating defaultValue={4} size="sm" /> */}
+                  <Link href="#">
+                    <Text fontSize="sm" fontWeight="medium" color={useColorModeValue("gray.600", "gray.400")}>
+                      {asset.collection && (
+                        <Link href={`/collections/${asset.collection.chainId}/${asset.collection.contractAddress}`}>
+                          {asset.collection.name}
+                        </Link>
+                      )}
+                      {" #" + asset.tokenId}
+                    </Text>
+                  </Link>
+                </HStack>
+                <Heading size="lg" fontWeight="medium">
+                  {asset.name}
+                </Heading>
+              </Stack>
+              {/* <PriceTag price={229} currency="GBP" rootProps={{ fontSize: 'xl' }} /> */}
+              <Text color={useColorModeValue("gray.600", "gray.400")}>{asset.description}</Text>
+            </Stack>
+            <Stack direction={{ base: "column", md: "row" }} spacing={{ base: "6", md: "8" }}>
+              <Stack flex="1">
+                <HStack spacing="1" color={useColorModeValue("gray.600", "gray.400")}>
+                  <Icon as={FiClock} />
+                  <Text fontSize="xs" fontWeight="medium">
+                    Low stock
+                  </Text>
+                </HStack>
+              </Stack>
+            </Stack>
+            <HStack spacing={{ base: "4", md: "8" }} align="flex-end" justify="space-evenly">
+              <Box flex="1">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  fontSize="md"
+                  width="full"
+                  leftIcon={<Icon as={FiHeart} boxSize="4" />}
+                >
+                  Favorite
+                </Button>
+              </Box>
+            </HStack>
+            <Button colorScheme="blue" size="lg">
+              Add to cart
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
     </Box>
   );
 };
