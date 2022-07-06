5f9e8fbba2ab53dff93f872364337ad12bfd6b4b
kenta suhara <ponta.blockchain@gmail.com>
2022-07-06 11:54:41 +0900

    fix: hooks

--- packages/web/src/components/molecules/NFTListItem/NFTListItem.tsx
+++ packages/web/src/components/molecules/NFTListItem/NFTListItem.tsx
@@ -12,18 +12,16 @@ export const NFTListItem: React.FC<NFTListItemProps> = ({ nft }) => {
     <Stack spacing="3">
       <Box position="relative" className="group">
         {nft.metadata && (
-          <>
-            <AspectRatio ratio={1}>
-              <Image src={nft.metadata.image} alt={nft.metadata.name} draggable="false" fallback={<Skeleton />} />
-            </AspectRatio>
-            <HStack spacing="3" position="absolute" top="4" left="4">
-              {/* TODO: change to chain name and color */}
-              <Tag key={nft.chainId} bg={`${"purple"}.500`} color="white" fontWeight="semibold">
-                {nft.chainId}
-              </Tag>
-            </HStack>
-          </>
+          <AspectRatio ratio={1}>
+            <Image src={nft.metadata.image} alt={nft.metadata.name} draggable="false" fallback={<Skeleton />} />
+          </AspectRatio>
         )}
+        <HStack spacing="3" position="absolute" top="4" left="4">
+          {/* TODO: change to chain name and color */}
+          <Tag key={nft.chainId} bg={`${"purple"}.500`} color="white" fontWeight="semibold">
+            {nft.chainId}
+          </Tag>
+        </HStack>
       </Box>
       {nft.collection && <Text>{nft.collection.name}</Text>}
       {nft.metadata && <Text fontWeight="semibold">{nft.metadata.name}</Text>}
--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,16 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <MoralisProvider
-        serverUrl={"https://zuik862kkoww.usemoralis.com:2053/server"}
-        appId={"Quc5FRSSDMHygFgnrjezoyGsfIaGQOgyOneXtttz"}
-      >
-        <Component {...pageProps} />
-      </MoralisProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
