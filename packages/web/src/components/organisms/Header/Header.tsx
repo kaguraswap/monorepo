c77cd50ac86d1ad3b36d83da6e81e9d374ca7c57
kenta suhara <ponta.blockchain@gmail.com>
2022-07-07 17:52:52 +0900

    fix: mobile view of navbar

--- packages/web/src/components/organisms/Header/Header.tsx
+++ packages/web/src/components/organisms/Header/Header.tsx
@@ -1,6 +1,5 @@
-import { Box, Flex, HStack, IconButton, Text, useBreakpointValue, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, HStack, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
-import { FiMenu } from "react-icons/fi";
 import { useAccount } from "wagmi";
 
 import { Link } from "../../atoms/Link";
@@ -9,7 +8,6 @@ import { ConnectWalletButton } from "../../molecules/ConnectWalletButton";
 import { DropdownSelectNetwork } from "../../molecules/DropdownSelectNetwork";
 
 export const Header: React.FC = () => {
-  const isDesktop = useBreakpointValue({ base: false, lg: true });
   const [{ data }] = useAccount();
   return (
     <Box as="section">
@@ -27,16 +25,10 @@ export const Header: React.FC = () => {
             <Link href="/">Logo</Link>
           </Text>
           <Flex gap={"1"}>
-            {isDesktop ? (
-              <>
-                <HStack spacing="3">
-                  <Box mr="2">{data ? <DropdownSelectNetwork /> : <></>}</Box>
-                  {data ? <ConnectedWallet /> : <ConnectWalletButton expectedChainId={1} />}
-                </HStack>
-              </>
-            ) : (
-              <IconButton variant="ghost" icon={<FiMenu fontSize="1.25rem" />} aria-label="Open Menu" />
-            )}
+            <HStack spacing="3">
+              {data ? <DropdownSelectNetwork /> : <></>}
+              {data ? <ConnectedWallet /> : <ConnectWalletButton expectedChainId={1} />}
+            </HStack>
           </Flex>
         </HStack>
       </Box>
