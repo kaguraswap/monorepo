5a4ff65da085e9c70cffeb9e3a0be405758774f0
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-08 08:17:13 +0900

    feat: resolve #82

--- packages/web/src/components/organisms/Footer/Footer.tsx
+++ packages/web/src/components/organisms/Footer/Footer.tsx
@@ -1,18 +1,37 @@
 import { Box, ButtonGroup, Divider, IconButton, Stack, Text } from "@chakra-ui/react";
 import React from "react";
-import { FaGithub, FaTwitter } from "react-icons/fa";
+import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
 
 export const Footer: React.FC = () => {
   return (
     <Box>
       <Divider />
-      <Stack pt="8" pb="12" justify="space-between" direction={{ base: "column-reverse", md: "row" }} align="center">
-        <Text fontSize="sm" color="subtle">
-          &copy; {new Date().getFullYear()} Chakra UI Pro, Inc. All rights reserved.
+      <Stack p="4" justify="space-between" direction={{ base: "column-reverse", md: "row" }} align="center">
+        <Text fontSize="xs" color="gray.800">
+          &copy; {new Date().getFullYear()} KaguraSwap
         </Text>
         <ButtonGroup variant="ghost">
-          <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="1.25rem" />} />
-          <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="1.25rem" />} />
+          <IconButton
+            as="a"
+            href="http://github.com/kaguraswap/monorepo"
+            target="_blank"
+            aria-label="GitHub"
+            icon={<FaGithub fontSize="1.25rem" />}
+          />
+          <IconButton
+            as="a"
+            href="https://discord.gg/tQnCDXUgP2"
+            target="_blank"
+            aria-label="Discord"
+            icon={<FaDiscord fontSize="1.25rem" />}
+          />
+          <IconButton
+            as="a"
+            href="http://twitter.com/kaguraswap"
+            target="_blank"
+            aria-label="Twitter"
+            icon={<FaTwitter fontSize="1.25rem" />}
+          />
         </ButtonGroup>
       </Stack>
     </Box>
