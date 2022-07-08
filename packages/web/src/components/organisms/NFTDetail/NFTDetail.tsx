f6651c1024b99668bda1dae57588c4e1ae83a209
kenta suhara <ponta.blockchain@gmail.com>
2022-07-08 15:47:23 +0900

    style: beatloader

--- packages/web/src/components/organisms/NFTDetail/NFTDetail.tsx
+++ packages/web/src/components/organisms/NFTDetail/NFTDetail.tsx
@@ -30,6 +30,7 @@ import { ethers } from "ethers";
 import { httpsCallable } from "firebase/functions";
 import React, { useState } from "react";
 import { FiExternalLink } from "react-icons/fi";
+import BeatLoader from "react-spinners/BeatLoader";
 import { useAccount, useSigner } from "wagmi";
 
 import { BSP, FEE_RECIPIENT } from "../../../../../common/configs/app";
@@ -123,7 +124,7 @@ export const NFTDetail: React.FC<NFTDetailProps> = ({ nft, orders }) => {
     <Box>
       <Box maxW="7xl" mx="auto" px={{ base: "4", md: "8", lg: "12" }} py={{ base: "6", md: "8", lg: "12" }}>
         <Stack direction={{ base: "column", lg: "row" }} spacing={{ base: "6", lg: "12", xl: "16" }}>
-          {nft.metadata ? (
+          {nft.metadata?.image ? (
             <Image src={nft.metadata.image} alt={nft.metadata.name} width={"xl"}></Image>
           ) : (
             <Image src="/image_placeholder.png" alt="placeholder" width={"xl"}></Image>
@@ -134,10 +135,20 @@ export const NFTDetail: React.FC<NFTDetailProps> = ({ nft, orders }) => {
                 <Text fontSize="sm" fontWeight="medium" color={useColorModeValue("gray.600", "gray.400")}>
                   {nft.chainId}
                 </Text>
-                {nft.metadata && (
-                  <Heading size="lg" fontWeight="medium">
-                    {nft.metadata.name}
-                  </Heading>
+                {nft.metadata ? (
+                  <>
+                    {nft.metadata.name ? (
+                      <Heading size="lg" fontWeight="medium">
+                        {nft.metadata.name}
+                      </Heading>
+                    ) : (
+                      <Heading size="lg" fontWeight="medium">
+                        # {nft.tokenId}
+                      </Heading>
+                    )}
+                  </>
+                ) : (
+                  <BeatLoader size={16} />
                 )}
               </Stack>
               <Text color={useColorModeValue("gray.600", "gray.400")}>
