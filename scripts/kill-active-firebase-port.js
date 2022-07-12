3a009f99c49ffc13ab09697e78e2bc562d6a8f51
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-12 21:40:53 +0900

    chore: improve dev process

--- scripts/kill-active-firebase-port.js
+++ scripts/kill-active-firebase-port.js
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const tcpPortUsed = require("tcp-port-used");
 const kill = require("kill-port");
 const firebase = require("../firebase.json");
 
-const main = () => {
+const main = async () => {
   for (const { port } of Object.values(firebase.emulators)) {
     if (!isNaN(port)) {
-      kill(port, "tcp").then(console.log).catch(console.log);
+      tcpPortUsed.check(port, "localhost").then((isUsed) => {
+        if (isUsed) {
+          kill(port, "tcp").then(console.log("killed:", port));
+        }
+      });
     }
   }
 };
